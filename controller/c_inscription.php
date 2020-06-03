70e955edf180d90ddc713f2b1211c71becea8557
ThomasLemoine <thomaslemoine@live.fr>
2020-06-03 14:19:01 +0400

    Update c_inscription.php

--- controller/c_inscription.php
+++ controller/c_inscription.php
@@ -6,7 +6,6 @@
         $stmt->execute(array('email_User' => $email));
         
         $result = $stmt->fetchAll();
-        print_r($result);
 
         if(count($result) == 1){
             return true;
@@ -21,19 +20,36 @@
         $result = $stmt->execute(array(
             'pseudo_User'         => $pseudo,
             'email_User'          => $mail,
-            'pass_User'           => $mdp,
+            'pass_User'           => $mdp
         ));
     }
-
-    $verification = verificationDoublon($pdo, htmlspecialchars($_POST['pseudo']), htmlspecialchars($_POST['email']) );
-    
-    if($verification){
-        $view = "";
-    }else{
-        inscription($pdo, htmlspecialchars($_POST['pseudo']), htmlspecialchars($_POST['email']), htmlspecialchars(md5($_POST['mdp'])) );
-        $view = "";
+    //on vérifie que les deux mdp sont identiques
+    function mdpIdentique($mdp1, $mdp2){
+        if($mdp1 == $mdp2){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    //on vérifie que l'email est un email
+    function verificationEmail($email){
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    $verificationMdp = mdpIdentique(htmlspecialchars(md5($_POST['mdp'])), htmlspecialchars(md5($_POST['mdp2'])));
+    $verificationEmail = verificationEmail(htmlspecialchars($_POST['email']));
+    $verificationDoublon = verificationDoublon($pdo, htmlspecialchars($_POST['pseudo']), htmlspecialchars($_POST['email']) );
+    
+    if($verificationDoublon || !$verificationEmail || !$verificationMdp){
+        $view = "";
+    }else{        
+        inscription($pdo, htmlspecialchars($_POST['pseudo']), htmlspecialchars($_POST['email']), htmlspecialchars(md5($_POST['mdp'])));
+        $view = "";
+    }
 
 
 ?>
