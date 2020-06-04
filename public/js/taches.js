05ab6e1521f8d63874c6fdc9ec078334d5000183
Darylabrador <abrador.daryl@gmail.com>
2020-06-04 12:02:55 +0400

    correction bug du côté d'Amélie

--- public/js/taches.js
+++ public/js/taches.js
@@ -42,18 +42,18 @@ function renduJSON(json) {
                             <p class="mb-1">${json[i].tache}</p>
                         </div>
                         <div class="col-1 d-flex align-items-center">
-                        <button id="modifier${i}" class="btn btn-outline-info" data-toggle="tooltip" data-placement="top" title="Modifier">
+                        <button id="modifier${i}" class="btn btn-outline-info btnAction" data-toggle="tooltip" data-placement="top" title="Modifier">
                             <i class="fas fa-pencil-alt"></i>
                         </button>
                             
                         </div>
                         <div class="col-1 d-flex align-items-center">
-                            <button id="validation${i}" class="btn btn-outline-success" data-toggle="tooltip" data-placement="top" title="Valider">
+                            <button id="validation${i}" class="btn btn-outline-success btnAction" data-toggle="tooltip" data-placement="top" title="Valider">
                                 <i class="fa fa-calendar-check-o"></i>
                             </button>
                         </div>
                         <div class="col-1 d-flex align-items-center">
-                            <button id="supprimer${i}" class="btn btn-outline-danger" data-toggle="tooltip" data-placement="top" title="Supprimer">
+                            <button id="supprimer${i}" class="btn btn-outline-danger btnAction" data-toggle="tooltip" data-placement="top" title="Supprimer">
                                  <i class="fas fa-trash-alt"></i>
                             </button>
                         </div>
@@ -110,7 +110,7 @@ function renduJSON(json) {
                             <p class="mb-1">${json[i].tache}</p>
                         </div>
                         <div class="col-1 d-flex align-items-center">
-                            <button id="validation${i}" class="btn btn-outline-success" data-toggle="tooltip" data-placement="top" title="Valider">
+                            <button id="validation${i}" class="btn btn-outline-success btnAction" data-toggle="tooltip" data-placement="top" title="Valider">
                                 <i class="fa fa-calendar-check-o" id="validation${i}"></i>
                             </button>
                         </div>
@@ -186,9 +186,10 @@ function recupDataModal(tache) {
             // Fonctionnalités présent si on est le créateur de la tâche
             if (utilisateur == reponse.tache[0].maitre) {
 
-                listeUtilisateur.innerHTML = "";
-
                 for (let j = 0; j < reponse.users.length; j++) {
+
+                    // On réinitialise à chaque fois
+                    listeUtilisateur.innerHTML = "";
 
                     // On crée les options du sélect
                     let utilisateurs = document.createElement('option');
@@ -204,12 +205,13 @@ function recupDataModal(tache) {
                         utilisateurs.textContent = reponse.users[j].pseudo;
                         listeUtilisateur.appendChild(utilisateurs);
 
-                    } else {
+                    } else if(reponse.tache[0].maitre != reponse.users[j].idUser){
                         let utilisateurs = document.createElement('option');
                         utilisateurs.value = reponse.users[j].idUser;
                         utilisateurs.textContent = reponse.users[j].pseudo;
                         listeUtilisateur.appendChild(utilisateurs);
                     }
+                    
                 }
             } else {
                 labelModif.remove();
