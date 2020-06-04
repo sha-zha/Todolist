70f167a3e33c2d93777b34afcaca8505b3e7f2ea
laurentPause <48903538+laurentPause@users.noreply.github.com>
2020-06-04 13:03:35 +0400

    mise en forme msg de connexion et inscription

--- php/view/v_inscription.php
+++ php/view/v_inscription.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <div class="container">
-        <div id="messageAction"><?php echo $msg;?></div>
+        <div class="text-center text-light" id="messageAction"><?php echo $msg;?></div>
         <div class="d-flex justify-content-center h-100">
             <div class="card">
                 <div class="card-header">
